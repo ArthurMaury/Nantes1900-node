cdedd5c510c5f3d03386d4569445c776a4d13fdc
Arthur Maury <arthur03.maury@orange.fr>
2017-10-24 14:01:18 +0200

    spatial data

--- index.js
+++ index.js
@@ -25,18 +25,20 @@ function getData(query) {
         })
 }
 
-///////////////// GETTING OBJECTS //////////////////////////
+//#region GETTING OBJECTS
 
 app.get('/Objects', (req, res) => {
     getObjects().then(data => {
-        res.send(data.slice(0, 10));
+        res.send(data); // HACK
     })
 })
 
 function getObjects() {
     const objectsColumns = ["object_id", "object_title", "object_abstract", "object_description", "object_version", "object_status"];
     const objectsQuery = `SELECT ${objectsColumns.join(',')} from object.object`;
-    return getData(objectsQuery).then(addKeywords);
+    return getData(objectsQuery)
+        .then(addKeywords)
+        .then(addSpatial);
 }
 
 function addKeywords(objects) {
@@ -66,7 +68,72 @@ function getKeywords(object) {
     })
 }
 
+function addSpatial(objects) {
+    var promises = [];
+    objects.forEach(object => {
+        promises.push(
+            getSpatialObject(object).then(spatial => {
+                if (spatial){
+                    Object.keys(spatial).forEach(prop => {
+                        object[prop] = spatial[prop];
+                    })
+                }
+                return object;
+            })
+        )
+    })
+    return Promise.all(promises).then(objectsWithSpatial => {
+        return objectsWithSpatial;
+    })
+}
 
-///////////////// GETTING RELATIONS //////////////////////////
+function getSpatialObject(object) {
+    const spatialColumns = ["spatial_geometry_mock_up", "spatial_zone", "spatial_adress_text", "spatial_latitude", "spatial_longitude"];
+    const spatialQuery = `SELECT ${spatialColumns.join(',')} from spatial.spatial
+                        INNER JOIN spatial.has_spatial ON spatial.has_spatial.has_spatial_spatial = spatial.spatial.spatial_id
+                        INNER JOIN object.object ON spatial.has_spatial.has_spatial_object = object.object.object_id
+                        WHERE object.object.object_id = ${object.object_id}`;
+    return getData(spatialQuery).then(spatials => {
+        return spatials[0];
+    })
+}
+
+//#endregion
 
 
+//#region GETTING RELATIONS
+
+app.get('/Relations', (req, res) => {
+    getRelations().then(data => {
+        res.send(data);
+    })
+})
+
+function getRelations() {
+    const objectsColumns = ["object_relation_first", "object_relation_second"];
+    const objectsQuery = `SELECT ${objectsColumns.join(',')}
+                            FROM object.object
+                            WHERE object_relation_first is not null`;
+    return getData(objectsQuery);
+}
+
+//#endregion
+
+
+
+//#region GETTING SPATIAL DATA
+
+app.get('/Spatial', (req, res) => {
+    getSpatialData().then(data => {
+        res.send(data);
+    })
+})
+
+function getSpatialData() {
+    const spatialColumns = ["has_spatial_object as object_id", "spatial_latitude", "spatial_longitude", "spatial_adress_text"];
+    const spatialQuery = `SELECT ${spatialColumns.join(',')} from spatial.spatial
+                        INNER JOIN spatial.has_spatial ON spatial.has_spatial.has_spatial_spatial = spatial.spatial.spatial_id`;
+    return getData(spatialQuery);
+}
+
+//#endregion
