dad0e16476aa12dc718dfede559ef9e7d417a037
Arthur Maury <arthur03.maury@orange.fr>
2017-11-23 00:44:32 +0100

    fix getObjects

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 const pg = require('pg');
 
-app.listen(3000, function () { })
+app.listen(3000, function () {})
 
 const config = fs.readFileSync("database_config.json")
 
@@ -34,68 +34,14 @@ app.get('/Objects', (req, res) => {
 
 function getObjects() {
     const objectsColumns = ["object_id Id", "object_title Title", "object_abstract Abstract", "object_version VersionNb", "object_status Status"];
-    const objectsQuery = `SELECT ${objectsColumns.join(',')} from object.object`;
-    return getData(objectsQuery)
-        .then(addSpatial);
-
-    /*return getData(objectsQuery)
-        .then(addKeywords)
-        .then(addSpatial);*/
-}
-
-function addKeywords(objects) {
-    var promises = [];
-    objects.forEach(object => {
-        promises.push(
-            getKeywords(object).then(keywords => {
-                object["Keywords"] = keywords.map(keyword => {
-                    return keyword.keyword_name;
-                });
-                return object;
-            })
-        )
-    })
-    return Promise.all(promises).then(objectsWithKeywords => {
-        return objectsWithKeywords;
-    })
-}
-
-function getKeywords(object) {
-    const keywordsQuery = `SELECT keyword_name from category.keyword
-                    INNER JOIN category.has_keyword ON category.has_keyword.has_keyword_keyword = category.keyword.keyword_id
-                    INNER JOIN object.object ON category.has_keyword.has_keyword_object = object.object.object_id 
-                    WHERE object.object.object_id = ${object.object_id} `;
-    return getData(keywordsQuery).then(keywords => {
-        return keywords;
-    })
-}
-
-function addSpatial(objects) {
-    var promises = [];
-    objects.forEach(object => {
-        promises.push(
-            getSpatialObject(object).then(spatial => {
-                if (spatial){
-                    Object.Geometry = spatial.Geometry
-                }
-                return object;
-            })
-        )
-    })
-    return Promise.all(promises).then(objectsWithSpatial => {
-        return objectsWithSpatial;
-    })
-}
-
-function getSpatialObject(object) {
-    const spatialColumns = ["spatial_geometry_mock_up Geometry", "spatial_latitude Latitude", "spatial_longitude Longitude"];
-    const spatialQuery = `SELECT ${spatialColumns.join(',')} from spatial.spatial
-                        INNER JOIN spatial.has_spatial ON spatial.has_spatial.has_spatial_spatial = spatial.spatial.spatial_id
-                        INNER JOIN object.object ON spatial.has_spatial.has_spatial_object = object.object.object_id
-                        WHERE object.object.object_id = ${object.id}`;
-    return getData(spatialQuery).then(spatials => {
-        return spatials[0];
-    })
+    const spatialColumns = ["ST_AsText(spatial_geometry_mock_up) Geometry", "spatial_latitude Latitude", "spatial_longitude Longitude"];
+    const objectsQuery = `SELECT ${objectsColumns.join(',')}, ${spatialColumns.join(',')} 
+                        FROM object.object
+                        LEFT JOIN spatial.has_spatial ON spatial.has_spatial.has_spatial_object = object.object.object_id
+                        LEFT JOIN spatial.spatial ON spatial.has_spatial.has_spatial_spatial = spatial.spatial.spatial_id
+                        WHERE object.object.object_title is not null
+                        ORDER BY object.object.object_id`
+    return getData(objectsQuery);
 }
 
 //#endregion
